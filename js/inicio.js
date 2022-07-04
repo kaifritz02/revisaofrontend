00045544390407c8f98605dd1bc615ec355f2ab1
kaifritz02 <kairon.lima1@gmail.com>
2022-07-04 11:57:07 -0300

    New animation improvements

---
+++ js/inicio.js
@@ -0,0 +1,15 @@
+//o que é uma variável?
+/*
+  A variável é um espaço reservado na memória
+  do computador.
+  Vamos declarar variáveis no JavaScript?
+*/
+
+const a = document.querySelector("sign");
+
+
+a.onclick = function() {
+ 
+    window.location="aula1.html";
+
+};
